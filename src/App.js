a95d05ea9e39a3ef64bd1f8816bc2c9f0b6d8004
Merto <mertenercan@gmail.com>
2023-01-03 03:35:41 +0300

    -refactored getNftData fn

--- src/App.js
+++ src/App.js
@@ -12,17 +12,21 @@ function App() {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner()
     const contract = new ethers.Contract("0x445965dE15b1ee2024D3F5a3655b3D1d2d909b69", abi.abi, signer)
-    await contract.mint("https://gateway.pinata.cloud/ipfs/QmeseZfShkgLos7MQgGxreaLMSq3BXtB5VtwEqEaiY1cMW/1.json")
+    await contract.mint("https://ipfs.io/ipfs/Qmerhz8zanVrWQgK5aLXU2HHYPFmrMmuLwYgcm8j7kE4UR/3.json")
     
   }
   const getNftData = async () => {
-    // const provider = new ethers.providers.Web3Provider(window.ethereum)
-    // const signer = provider.getSigner()
-    // const signerAddr = signer.getAddress()
-    // const contract = new ethers.Contract("0x445965dE15b1ee2024D3F5a3655b3D1d2d909b69", abi.abi, signer)
-    // const nftData = await contract.getAll(signerAddr)
-    // const nftMetaData = await contract.tokenURI(nftData[0].toNumber())
-    await axios('https://gateway.pinata.cloud/ipfs/QmRmA8Xadw2AK2THY51JuiVGVKA3Y4fgvXR9XukNTGfqJq?filename=4.png').then(res => console.log(res))
+    //1 vulpix, 2arcanine, 3misdreavus, 4pikachu
+    // working code await axios('https://ipfs.io/ipfs/QmeseZfShkgLos7MQgGxreaLMSq3BXtB5VtwEqEaiY1cMW/3.json').then(res => console.log(res.data))
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const signerAddr = signer.getAddress()
+    const contract = new ethers.Contract("0x445965dE15b1ee2024D3F5a3655b3D1d2d909b69", abi.abi, signer)
+    const nftData = await contract.getAll(signerAddr)
+    const nftMetaData = await contract.tokenURI(nftData[5])
+    const nftImage = await axios(nftMetaData).then(res => res.data.properties.image.value)
+    await setNftImg(nftImage)
+    
   }
   console.log(nftMetadata)
   return (
